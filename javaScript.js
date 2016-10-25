26ef1663b94639072e4b162c36329c9273b0a902
wlgfour <wlgfour@gmail.com>
2016-10-25 18:14:58 -0400

    made fixed js betetr

--- javaScript.js
+++ javaScript.js
@@ -1,30 +1,12 @@
 function addGraph() {
 
 	var input = document.createElement('input');
-	input.id = 'graphName';
 	var button = document.createElement('button');
-	button.class = 'btn btn-default';
 	button.innerHTML = 'Enter';
-	button.onclick = buttonPressed;
-	var form = document.createElement('div');
-	form.id = 'graphForm';
-	form.appendChild(input);
-	form.appendChild(button);
-	document.getElementById('graphContainer').appendChild(form);
-
-
-	document.getElementById.appendChild(form);
-
-	function buttonPressed() {
-		var name = document.getElementById('graphName').value;
-		var indata = document.getElementById('graphForm');
-		indata.parentNode.removeChild(indata);
-		createGraph(name);
-	}
-
-	function createGraph(graphName) {
+	button.onclick = function() {
+		var graphName = input.value;
+		form.parentNode.removeChild(form);
 		var div = document.createElement('div');
-		//div.id = graphName;
 		div.style.width = '50%';
 		var t1 = {
 			x:[1,2,3],
@@ -38,5 +20,10 @@ function addGraph() {
 		};
 		Plotly.newPlot(div, data, layout);
 		document.getElementById('graphContainer').appendChild(div);
-	}
+	};
+	var form = document.createElement('div');
+	form.appendChild(input);
+	form.appendChild(button);
+	document.getElementById('graphContainer').appendChild(form);
+
 }
