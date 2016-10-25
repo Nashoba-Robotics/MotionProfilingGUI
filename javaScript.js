0c4ba7f3688d1d57facbb07440ab5b04ab792bf1
William L Gerecke IV <2018wgereckeiv@nrsd.net>
2016-10-25 16:17:54 -0400

    fixed some js

--- javaScript.js
+++ javaScript.js
@@ -1,25 +1,31 @@
 function addGraph() {
-	var form = document.createElement('div');
+
 	var input = document.createElement('input');
+	input.id = 'graphName';
 	var button = document.createElement('button');
 	button.class = 'btn btn-default';
 	button.innerHTML = 'Enter';
-	button.onclick = 'addGraph().buttonPressed()';//currently not calling function or function not working
+	button.onclick = buttonPressed;
+	var form = document.createElement('div');
+	form.id = 'graphForm';
 	form.appendChild(input);
 	form.appendChild(button);
 	document.getElementById('graphContainer').appendChild(form);
 
 
 	document.getElementById.appendChild(form);
+
 	function buttonPressed() {
-		createGraph(document.getElementsByName(graphName).value);
+		var name = document.getElementById('graphName').value;
+		var indata = document.getElementById('graphForm');
+		indata.parentNode.removeChild(indata);
+		createGraph(name);
 	}
+
 	function createGraph(graphName) {
 		var div = document.createElement('div');
-		document.getElementById('graphContainer').appendChild(div);
-		div.id = graphName;
+		//div.id = graphName;
 		div.style.width = '50%';
-		//div.style.backgroundColor = 'red';
 		var t1 = {
 			x:[1,2,3],
 			y:[1,-2,3],
@@ -27,16 +33,10 @@ function addGraph() {
 		};
 		var data = [t1];
 		var layout = {
-			title:'TestGraph',
+			title: graphName,
 			width:'100%',
 		};
-		Plotly.newPlot(graphName, data, layout);
+		Plotly.newPlot(div, data, layout);
+		document.getElementById('graphContainer').appendChild(div);
 	}
 }
-
-/*
-<div>
-	Graph Name: <input type='text' name='graphName'><br>
-	<button onpress='addGraph.buttonPressed()' type='button' class='btn btn-default'>Enter</button>
-</div>
-*/
