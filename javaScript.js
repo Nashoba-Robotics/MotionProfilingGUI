2fc700123d2bb7134b95493539e864cadd05e0f7
Robotics Laptop <1768programming@gmail.com>
2016-10-28 15:59:30 -0400

    Javascript butons for doing graphs created

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,6 @@
 function addGraph() {
 
+	//Button to name graph
 	var input = document.createElement('input');
 	var button = document.createElement('button');
 	button.innerHTML = 'Enter';
@@ -9,8 +10,8 @@ function addGraph() {
 		var div = document.createElement('div');
 		div.style.width = '50%';
 		var t1 = {
-			x:[1,2,3],
-			y:[1,-2,3],
+			x:[],
+			y:[],
 			mode:'lines'
 		};
 		var data = [t1];
@@ -26,4 +27,26 @@ function addGraph() {
 	form.appendChild(button);
 	document.getElementById('graphContainer').appendChild(form);
 
+	//Initializes delete, hide, and show buttons above graph
+	var button = document.createElement('dlte');
+	var button = document.createElement('hide');
+	var button = document.createElement('show');
+
+	//Function for delete button
+	dlte.innerHTML = 'Delete Graph';
+	dlte.onclick = function() {
+
+	}
+	
+	//Function for hide graph
+	hide.innerHTML = 'Hide Graph';
+	hide.onclick = function() {
+
+	}
+	
+	//Function for show graph
+	show.innerHTML = 'Show graph';
+	show.onclick = function() {
+		
+	}
 }
